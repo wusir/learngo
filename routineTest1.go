63a797568327f6ab546c15795164068eb4387b08
ericghw <ericghw@163.com>
2018-02-08 13:28:49 +0800

    test go routine

---
+++ routineTest1.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+import "time"
+import "math/rand"
+
+func routine(name string, delay time.Duration){
+	t0:= time.Now()
+	fmt.Println(name, "开始于 ", t0)
+
+	time.Sleep(delay)
+
+	t1:= time.Now()
+	fmt.Println(name, "结束于 ", t1)
+
+	fmt.Println(name, "共执行 ", t1.Sub(t0))
+}
+
+func main(){
+	//生成随机种子
+	rand.Seed(time.Now().Unix())
+
+	var name string
+	for i:=0; i<3;i++{
+		//生成名称
+		name = fmt.Sprintf("go_%d", i)
+
+		go routine(name, time.Duration(rand.Intn(5)) * time.Second)
+	}
+
+	//主进程等待
+	var input string
+	fmt.Scanln(&input)
+	fmt.Println("done")
+}
